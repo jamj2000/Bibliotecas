baed82221800a6e671970ab6a1a9f112e311fc9f
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2017-10-16 00:20:43 +0200

    Update aritmetica.c

--- c/aritmetica.c
+++ c/aritmetica.c
@@ -1,18 +1,15 @@
-int suma (int sumando1, int sumando2){
+int suma (int sumando1, int sumando2) {
 	return (sumando1+sumando2);
 }	
 
-int resta  (int minuendo, int sustraendo)
-{
+int resta  (int minuendo, int sustraendo) {
 	return (minuendo-sustraendo);
 }
 
-int multiplicacion (int  numero1, int numero2)
-{
+int multiplicacion (int  numero1, int numero2) {
 	return (numero1*numero2);
 }
 
-float division (int dividendo, int divisor)
-{
+float division (int dividendo, int divisor) {
 	return (dividendo/divisor);
 }
